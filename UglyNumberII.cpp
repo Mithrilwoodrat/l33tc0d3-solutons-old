9766d64e2af0c752e56eae05748a952ffb51602d
mithrilwoodrat <mithrilwoodrat@gmail.com>
2019-04-30 15:23:47 +0800

    upload

--- UglyNumberII.cpp
+++ UglyNumberII.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-class Solution {
+class Solutionold {
 public:
     vector<int> uglynums {2,3,4,5,6,8,9,10,12};
     bool isUgly(int num) {
@@ -51,6 +51,27 @@ public:
             }
         }
         return 0;
+    }
+};
+
+// min(1 * (2,3,5))
+// min( (1,2) * (2,3,5) )
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        if (1 == n)
+            return 1;
+        int p1=0, p2=0, p3=0;
+        vector<int> k(n);
+        k[0] = 1;
+        for(int i =1;i<n;i++) {
+            k[i] = min(k[p1]*2, min(k[p2]*3, k[p3]*5));
+            if (k[i] == k[p1]*2) p1++;
+            if (k[i] == k[p2]*3) p2++;
+            if (k[i] == k[p3]*5) p3++;
+        }
+        return k[n-1];
     }
 };
 
